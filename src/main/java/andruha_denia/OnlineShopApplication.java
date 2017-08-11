853e96bb3c27bd0cb6a30c3835664ff0709879ef
andrulban <tytarchuk.andrey@gmail.com>
2017-08-11 23:45:34 +0300

    init

---
+++ src/main/java/andruha_denia/OnlineShopApplication.java
@@ -0,0 +1,12 @@
+package andruha_denia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopApplication.class, args);
+	}
+}
